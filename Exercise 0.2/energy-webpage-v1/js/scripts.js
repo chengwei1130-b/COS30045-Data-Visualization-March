41f8c563d3e1c9c4ba8515eeca7cb35746aba96f
chengwei1130-b <j23040103@student.newinti.edu.my>
2026-04-10 11:27:13 +0800

    Update scripts.js

--- Exercise 0.2/energy-webpage-v1/js/scripts.js
+++ Exercise 0.2/energy-webpage-v1/js/scripts.js
@@ -3,25 +3,31 @@ const pages = document.querySelectorAll(".page");
 const logo = document.querySelector(".logo");
 
 function showPage(pageId) {
+
+  // Hide all of the active page
   pages.forEach(page => {
     page.classList.remove("active-page");
   });
 
+  // Remove active highlight for nav link
   navLinks.forEach(link => {
     link.classList.remove("active");
   });
 
+  // Find the selected page using its ID
   const selectedPage = document.getElementById(pageId);
   if (selectedPage) {
-    selectedPage.classList.add("active-page");
+    selectedPage.classList.add("active-page"); // Show selected page
   }
 
+  // Find the corresponding nav link and highlight it
   const activeLink = document.querySelector(`.nav-links a[data-page="${pageId}"]`);
   if (activeLink) {
     activeLink.classList.add("active");
   }
 }
 
+// Add click event to each nav link
 navLinks.forEach(link => {
   link.addEventListener("click", function (event) {
     event.preventDefault();
@@ -30,8 +36,10 @@ navLinks.forEach(link => {
   });
 });
 
+
 logo.addEventListener("click", function () {
   showPage("home");
 });
+
 
 showPage("home");
